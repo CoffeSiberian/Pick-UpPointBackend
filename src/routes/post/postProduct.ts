3bb592be2d2ffd83a5c988ebf3e29cf01593d05f
Fernando <siberiancoffe@outlook.cl>
2023-11-29 19:03:03 -0300

    Add stock creation when creating a new product

--- src/routes/post/postProduct.ts
+++ src/routes/post/postProduct.ts
@@ -1,6 +1,7 @@
 import { Request, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { createProduct } from "../../repositories/ProductsR";
+import { createStock } from "../../repositories/StocksR";
 import { productSchema } from "../../schemas/ProductsSch";
 import { InfoResponse } from "../../utils/InfoResponse";
 import { ResponseJwt } from "../../types/ResponseExtends";
@@ -18,10 +19,18 @@ export const postProduct = async (
     }
 
     const body = value as ProductPost;
-    const Product = { id: uuidv4(), ...body };
+    const productId = uuidv4();
+    const Product = { id: productId, ...body };
+    const Stock = {
+        id: uuidv4(),
+        quantity: 0,
+        fk_store: res.jwtPayload.fk_store,
+        fk_product: productId,
+    };
 
     try {
         await createProduct(Product, res.jwtPayload.fk_store);
+        await createStock(Stock);
         res.status(200).json(InfoResponse(200, "Created"));
         return next();
     } catch (err: any) {

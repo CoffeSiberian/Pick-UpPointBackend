2c0f2325b6e839fe7ede1aca71f5f8c0e9b9737a
Fernando <siberiancoffe@outlook.cl>
2023-12-02 20:42:39 -0300

    Update product schema validation

--- src/schemas/ProductsSch.ts
+++ src/schemas/ProductsSch.ts
@@ -7,23 +7,24 @@ interface ProductsAndStocks extends Products {
 
 export const productSchema = Joi.object<ProductsAndStocks>({
     name: Joi.string().min(1).max(256).required(),
-    description: Joi.string().min(1).max(256).required(),
-    price: Joi.number().min(0).max(99999999999999999999).required(),
-    stock: Joi.number().min(0).max(99999999999999999999).required(),
+    description: Joi.string().min(1).max(2950).required(),
+    price: Joi.number().min(0).max(9999999999999).required(),
+    stock: Joi.number().min(0).max(9999999999999).required(),
     fk_category: Joi.string().uuid().required(),
 });
 
 export const productUpodateSchemaCustom = Joi.object({
     id: Joi.string().uuid().required(),
     name: Joi.string().min(1).max(256).required(),
-    description: Joi.string().min(1).max(256).required(),
-    price: Joi.number().min(0).max(99999999999999999999).required(),
+    description: Joi.string().min(1).max(3000).required(),
+    price: Joi.number().min(0).max(100000000).required(),
+    stock: Joi.number().min(0).max(100000000).required(),
     fk_category: Joi.string().uuid().required(),
 });
 
 export const productUpdateStockSchemaCustom = Joi.object({
     fk_product: Joi.string().uuid().required(),
-    quantity: Joi.number().min(0).max(99999999999999999999).required(),
+    quantity: Joi.number().min(0).max(100000000).required(),
 });
 
 export const buyProcessSchema = Joi.object({
